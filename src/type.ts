15da52f467c8f95803f8e146cca7a42dca4a8ad6
lihaha <i@lihaha.cn>
2024-04-01 21:12:19 +0800

    feat: cos

---
+++ src/type.ts
@@ -0,0 +1,12 @@
+export type InputsType = {
+  secretId: string;
+  secretKey: string;
+  bucket: string;
+  region: string;
+  localPath: string;
+  remotePath: string;
+  uploadOrderSuffix: string;
+  cdnDomain: string;
+  cdnRefresh: boolean;
+  cdnRefreshSuffix: string;
+}
